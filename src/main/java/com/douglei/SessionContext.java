fa5558d9bdf7d602db0575b28cbe832e1144b83a
DougLei <953034206@qq.com>
2019-06-05 18:18:02 +0800

    t

--- src/main/java/com/douglei/SessionContext.java
+++ src/main/java/com/douglei/SessionContext.java
@@ -1,5 +1,6 @@
 package com.douglei;
 
+import com.douglei.core.dialect.TransactionIsolationLevel;
 import com.douglei.sessions.Session;
 
 /**
@@ -10,13 +11,39 @@ public class SessionContext {
 	private static final ThreadLocal<Session> SESSION = new ThreadLocal<Session>();
 	
 	/**
-	 * 获取session
+	 * <pre>
+	 * 	开启Session实例
+	 * 	默认开启事物
+	 * </pre>
 	 * @return
 	 */
 	public static Session getSession() {
+		return getSession(true);
+	}
+	
+	/**
+	 * <pre>
+	 * 	开启Session实例
+	 * </pre>
+	 * @param beginTransaction 是否开启事物
+	 * @return
+	 */
+	public static Session getSession(boolean beginTransaction) {
+		return getSession(beginTransaction, null);
+	}
+	
+	/**
+	 * <pre>
+	 * 	开启Session实例
+	 * </pre>
+	 * @param beginTransaction 是否开启事物
+	 * @param transactionIsolationLevel 事物隔离级别, 如果传入null, 则使用jdbc默认的隔离级别
+	 * @return
+	 */
+	public static Session getSession(boolean beginTransaction, TransactionIsolationLevel transactionIsolationLevel) {
 		Session session = SESSION.get();
 		if(session == null) {
-			session = SessionFactoryContext.getSessionFactory().openSession();
+			session = SessionFactoryContext.getSessionFactory().openSession(beginTransaction, transactionIsolationLevel);
 			SESSION.set(session);
 		}
 		return session;

99586c24abca8ffda82309a37cc6a865e807ebca
DougLei <953034206@qq.com>
2019-06-06 19:17:29 +0800

    t

--- src/test/java/com/douglei/proxy/ITestProxy.java
+++ src/test/java/com/douglei/proxy/ITestProxy.java
@@ -1,7 +1,10 @@
 package com.douglei.proxy;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+
+import com.douglei.transaction.Transaction;
 
 public class ITestProxy implements InvocationHandler{
 	
@@ -13,6 +16,9 @@ public class ITestProxy implements InvocationHandler{
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 		System.out.println("begin");
+		
+		System.out.println(method.getAnnotation(Transaction.class));
+		
 		Object result = method.invoke(targetObject, args);
 		System.out.println("end");
 		return result;
--- src/test/java/com/douglei/proxy/Test.java
+++ src/test/java/com/douglei/proxy/Test.java
@@ -3,8 +3,11 @@ package com.douglei.proxy;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 
+import com.douglei.transaction.Transaction;
+
 public class Test implements ITest {
 
+	@Transaction
 	@Override
 	public void add() {
 		System.out.println("add method");
@@ -13,14 +16,12 @@ public class Test implements ITest {
 	public static void main(String[] args) {
 		InvocationHandler proxy = new ITestProxy(t);
 		
-		Class<?>[] ints = ITest2.class.getInterfaces();
-		System.out.println(ints.length);
-		for (Class<?> class1 : ints) {
-			System.out.println(class1.getName());
-		}
 		
 		ITest test = (ITest) Proxy.newProxyInstance(ITest.class.getClassLoader(), ITest2.class.getInterfaces(), proxy);
 		test.add();
+		
+		System.out.println(new Object().getClass().getEnclosingMethod().getName());
+		
 	}
 	private static Test t = new Test();
 }

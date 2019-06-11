84a9ea0166387e96302e17cc7cef1f7a87e0bbcb
DougLei <953034206@qq.com>
2019-06-11 11:01:34 +0800

    t

--- src/main/java/com/douglei/orm/context/TransactionAnnotationMemoryUsage.java
+++ src/main/java/com/douglei/orm/context/TransactionAnnotationMemoryUsage.java
@@ -1,21 +1,25 @@
 package com.douglei.orm.context;
 
+import com.douglei.orm.context.exception.RepeatedRecordTransactionAnnotationMemoryUsageException;
+
 /**
  * Transaction注解的使用情况, 即记录是否使用Transaction注解
  * @author DougLei
  */
 public class TransactionAnnotationMemoryUsage {
+	private static boolean isRecord;
 	private static boolean use;
 	private static boolean unUse;
 	
 	/**
-	 * <pre>
-	 * 	记录Transaction注解的使用情况
-	 * 	该方法很重要, 禁止随意调用
-	 * </pre>
+	 * 记录Transaction注解的使用情况
 	 * @param isUse
 	 */
 	public static void setMemoryUsage(boolean isUse) {
+		if(isRecord) {
+			throw new RepeatedRecordTransactionAnnotationMemoryUsageException();
+		}
+		isRecord = true;
 		use = isUse;
 		unUse = !isUse;
 	}
---
+++ src/main/java/com/douglei/orm/context/exception/RepeatedRecordTransactionAnnotationMemoryUsageException.java
@@ -0,0 +1,15 @@
+package com.douglei.orm.context.exception;
+
+import com.douglei.orm.context.TransactionAnnotationMemoryUsage;
+
+/**
+ * 重复记录Transaction注解使用情况异常
+ * @author DougLei
+ */
+public class RepeatedRecordTransactionAnnotationMemoryUsageException extends RuntimeException{
+	private static final long serialVersionUID = 6237556864796587201L;
+
+	public RepeatedRecordTransactionAnnotationMemoryUsageException() {
+		super("禁止通过["+TransactionAnnotationMemoryUsage.class.getName()+"]类, 重复setMemoryUsage");
+	}
+}

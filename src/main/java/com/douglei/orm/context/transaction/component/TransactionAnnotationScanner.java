071b702554ff267193b282818c77eae048258e7a
DougLei <953034206@qq.com>
2021-01-29 23:00:55 +0800

    t

--- src/main/java/com/douglei/orm/context/transaction/component/TransactionAnnotationScanner.java
+++ src/main/java/com/douglei/orm/context/transaction/component/TransactionAnnotationScanner.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import com.douglei.tools.instances.resource.scanner.impl.ClassScanner;
-import com.douglei.tools.utils.reflect.ClassLoadUtil;
+import com.douglei.tools.utils.reflect.ClassUtil;
 
 /**
  * Transaction注解扫描器
@@ -30,7 +30,7 @@ public class TransactionAnnotationScanner {
 				TransactionComponent transactionComponent = null;
 				TransactionComponentEntity entity = null;
 				for (String classpath : classpaths) {
-					clazz = ClassLoadUtil.loadClass(classpath);
+					clazz = ClassUtil.loadClass(classpath);
 					transactionComponent = clazz.getAnnotation(TransactionComponent.class);
 					
 					if(transactionComponent != null) {
